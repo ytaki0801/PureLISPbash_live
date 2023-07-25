3e5d607e672dcec7ddd9424070112ec4861a3510
TAKIZAWA Yozo <y@nbk.bz>
2023-07-26 01:03:29 +0900

    Added files for Part 02

--- car-cdr-cons.bash
+++ car-cdr-cons.bash
@@ -2,12 +2,11 @@ car () { echo ${1%% *}; }
 cdr () { echo ${1#* }; }
 cons () { echo "${1} ${2}"; }
 
-L0="a 1 c"
-L1="x y z"
+# (a (x y z) c)
+L[0]="a 1 c"
+L[1]="x y z"
 
-R=$(eval echo \${L$(car "$(cdr "${L0}")")})
+R="$(cdr "${L[$(car "$(cdr "${L[0]}")")]}")"
 
-echo ${L0}
 echo ${R}
-echo $(cdr "${R}")
 
