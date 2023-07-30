96ae5b6b918165da4b13d823540a7f76a6140a8e
TAKIZAWA Yozo <y@nbk.bz>
2023-07-30 22:44:46 +0900

    Results in Part 03

--- car-cdr-cons.bash
+++ car-cdr-cons.bash
@@ -1,12 +1,30 @@
-car () { echo ${1%% *}; }
+car () {
+  CARR=${1%% *};
+  if [[ ${CARR} =~ ^[0-9]+$ ]]; then
+    echo ${L[${CARR}]}
+  else
+    echo ${CARR}
+  fi
+}
 cdr () { echo ${1#* }; }
 cons () { echo "${1} ${2}"; }
 
 # (a (x y z) c)
-L[0]="a 1 c"
-L[1]="x y z"
+L[0]="a 1 c NULL"
+L[1]="x y z NULL"
 
-R="$(cdr "${L[$(car "$(cdr "${L[0]}")")]}")"
+# (car (cdr (car (cdr '(a (x y z) c))))) => y
+#R="${L[0]}"
+#R="$(cdr "${R}")"
+#R="$(car "${R}")"
+#R="$(cdr "${R}")"
+#R="$(car "${R}")"
+#echo ${R}
 
+# (cons 'h (car (cdr '(a (x y z) c))))
+R="${L[0]}"
+R="$(cdr "${R}")"
+R="$(car "${R}")"
+R="$(cons "h" "${R}")"
 echo ${R}
 
